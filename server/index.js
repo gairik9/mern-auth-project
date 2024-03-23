a49916ada060a6c1a2d105d23e70fcf8e7e9418f
gairik9 <gairik9@gmail.com>
2024-03-23 21:35:33 +0530

    Added .env file. Connected to DB.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const PORT = 8080;
+// Initialize  environment variables.
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected To DB.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const PORT = process.env.PORT_NUMBER || 3000;
 const app = express();
 
 app.listen(PORT, () => {
